16b3357c613a17b02d3847b9048e4f951bd5929e
kmc7468 <kmc7468@naver.com>
2017-04-26 22:08:48 +0900

    Command

--- LibLow/LibLow.hh
+++ LibLow/LibLow.hh
@@ -1,4 +1,10 @@
 #ifndef LIBLOW_HEADER_LIBLOW_HH
 #define LIBLOW_HEADER_LIBLOW_HH
 
+#include "LibLow/Command.hh"
+
+#ifdef LIBLOW_SHORT_NAMESPACE
+namespace LL = LibLow;
+#endif
+
 #endif
---
+++ LibLow/LibLow/Command.hh
@@ -0,0 +1,16 @@
+#ifndef LIBLOW_HEADER_COMMAND_HH
+#define LIBLOW_HEADER_COMMAND_HH
+
+namespace LibLow
+{
+	class Command final
+	{
+	public:
+		enum Type
+		{
+			Nop = 0,
+		};
+	};
+}
+
+#endif
