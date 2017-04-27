10e112ce8635e1aaac4aedeebf087e755e678c57
kmc7468 <kmc7468@naver.com>
2017-04-27 17:14:52 +0900

    Version Macro

--- LibLow/LibLow.hh
+++ LibLow/LibLow.hh
@@ -1,7 +1,13 @@
 #ifndef LIBLOW_HEADER_LIBLOW_HH
 #define LIBLOW_HEADER_LIBLOW_HH
 
+#define LIBLOW_VER 1
+#define LIBLOW_VER_MINOR 0
+#define LIBLOW_VER_FIX 0
+#define LIBLOW_VER_STRING "1.0.0"
+
 #include "LibLow/Command.hh"
+#include "LibLow/Object.hh"
 
 #ifdef LIBLOW_SHORT_NAMESPACE
 namespace LL = LibLow;
