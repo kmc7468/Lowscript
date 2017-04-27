2cb5ef99d25fea817e03306e61785ee1f936f902
kmc7468 <kmc7468@naver.com>
2017-04-27 17:24:53 +0900

    Double

---
+++ LibLow/LibLow/Types/Double.hh
@@ -0,0 +1,39 @@
+#ifndef LIBLOW_HEADER_TYPES_DOUBLE_HH
+#define LIBLOW_HEADER_TYPES_DOUBLE_HH
+
+#include "../Object.hh"
+
+namespace LibLow
+{
+	namespace Types
+	{
+		class Double final : public Object
+		{
+		public:
+			Double() = default;
+			Double(const double& value);
+			Double(const Double& object);
+			Double(Double&& object) noexcept;
+			~Double() = default;
+
+		public:
+			Double& operator=(const Double& object);
+			Double& operator=(Double&& object) noexcept;
+			bool operator==(const Double& object) const noexcept;
+			bool operator!=(const Double& object) const noexcept;
+
+		public:
+			virtual LibLow::Type Type() const noexcept override;
+			virtual SignedOption Signed() const noexcept override;
+
+		public:
+			double Value() const;
+			double& Value();
+
+		private:
+			double Value_ = 0.0;
+		};
+	}
+}
+
+#endif
---
+++ LibLow/Types/Double.cc
@@ -0,0 +1,56 @@
+#include "../LibLow/Types/Double.hh"
+
+#include <utility>
+
+namespace LibLow
+{
+	namespace Types
+	{
+		Double::Double(const double& value)
+			: Value_(value)
+		{}
+		Double::Double(const Double& object)
+			: Value_(object.Value_)
+		{}
+		Double::Double(Double&& object) noexcept
+			: Value_(std::move(object.Value_))
+		{}
+
+		Double& Double::operator=(const Double& object)
+		{
+			Value_ = object.Value_;
+			return *this;
+		}
+		Double& Double::operator=(Double&& object) noexcept
+		{
+			Value_ = std::move(object.Value_);
+			return *this;
+		}
+		bool Double::operator==(const Double& object) const noexcept
+		{
+			return Value_ == object.Value_;
+		}
+		bool Double::operator!=(const Double& object) const noexcept
+		{
+			return Value_ != object.Value_;
+		}
+
+		Type Double::Type() const noexcept
+		{
+			return Type::Double;
+		}
+		SignedOption Double::Signed() const noexcept
+		{
+			return SignedOption::Signed;
+		}
+
+		double Double::Value() const
+		{
+			return Value_;
+		}
+		double& Double::Value()
+		{
+			return Value_;
+		}
+	}
+}
